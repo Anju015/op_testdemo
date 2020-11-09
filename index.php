571f25e3723df73c275c914c7dfeffcdd7850eec
Anju015 <62645267+Anju015@users.noreply.github.com>
2020-11-08 23:23:32 -0800

    index as home page

---
+++ index.php
@@ -0,0 +1,159 @@
+<!-home page->
+<link rel="shortcut icon" href="images/fevicon.ico.png" type="image/x-icon" />
+   <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="css/bootstrap.min.css">
+   <!-- Site CSS -->
+   <link rel="stylesheet" href="style.css">
+   <!-- Colors CSS -->
+   <link rel="stylesheet" href="css/colors.css">
+   <!-- ALL VERSION CSS -->
+   <link rel="stylesheet" href="css/versions.css">
+   <!-- Responsive CSS -->
+   <link rel="stylesheet" href="css/responsive.css">
+   <!-- Custom CSS -->
+   <link rel="stylesheet" href="css/custom.css">
+   <!-- Modernizer for Portfolio -->
+   <script src="js/modernizer.js"></script>
+   <!-- [i...................-------------------
+
+
+
+
+
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<!-- mobile metas -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="initial-scale=1, maximum-scale=1">
+<!-- site metas -->
+<title>chc_puthenchira</title>
+<meta name="keywords" content="">
+<meta name="description" content="">
+<meta name="author" content="">
+<!-- bootstrap css -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<!-- style css -->
+<link rel="stylesheet" href="css/style.css">
+<!-- Responsive-->
+<link rel="stylesheet" href="css/responsive.css">
+<!-- fevicon -->
+<link rel="icon" href="images/fevicon.png" type="image/gif" />
+<!-- Scrollbar Custom CSS -->
+<link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+<!-- Tweaks for older IEs-->
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+
+<!doctype html>
+
+<link rel="stylesheet" type="text/css" href="ccs/home.css">
+<html>
+<head>
+<meta charset="utf-8">
+<title>chc puthenchira</title>
+</head>
+
+<body background="images/b2.jpg">
+<div class="body" >
+	<div class="top1">
+		<div class="logo">
+			<img src="pics/healthcare-bubble.jpg">
+		</div>
+		<div class="head">
+			<center>
+				<b style="color:brown;font-size: 30px;">CHC PUTHENCHIRA OP MANAGEMENT</b>
+			</center>
+		</div>
+<!---heading----->
+<!------menu bar--->
+		<div class="tab">
+			<div class="tab1"><b><a href="index.php">HOME</a></b></div>
+			<div class="tab1"><b><a href="aboutus.php">ABOUT US</a></b></div>
+			<div class="tab1"><b><a href="doctors.php">DOCTORS</a></b></div>
+			<div class="tab1"><b><a href="services.php"> SERVICES</a></b></div>
+			<div class="tab1"><b><a href="contactus.php">CONTACT US</a></b></div>
+			<div class="tab1"><b><a href="Login.php">LOGIN</a></b></div>
+		</div>
+  	</div>
+<!------ slider--->
+	<div class="body1">
+	 	<div class="part" style="padding-left:20px;">
+			<div class="item ">
+              <img  src="images/healthcare.jpg" alt="" height="500" width="1950">
+              <div class="container">
+				 <div class="carousel-caption">
+                  <h2 class="carousel-title bounceInDown animated slow" style="color:mediumvioletred;font-family: 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', 'DejaVu Sans', Verdana, 'sans-serif';font-size: 45px;">
+					  Healing in the broadest sense.
+				  </h2>
+                  <h4 class="carousel-subtitle bounceInUp animated slow" style="color:mediumvioletred;font-family: 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', 'DejaVu Sans', Verdana, 'sans-serif';font-size: 45px;">
+					  Health for Life. !
+				  </h4>
+                </div>
+              </div>
+            </div>
+		</div>
+<!--------images---->
+	<div class="about" style="padding-left:30px;">
+		
+		
+		<div class="box1">
+			<div class="small_content">
+				<center>
+				<p style="color: aliceblue;font-size: 20px;" >Paliative Care</p>
+			</center>
+				<img src="images/paliative_care.png" height="800px" width="500px">
+				<p style="color: aliceblue;font-size: 15px;" >
+					 Our goal is to improve quality of life for both the patient and the family.
+				</p>
+			</div>			
+		</div>
+		<div class="box1">
+			<div class="small_content">
+				<center>
+				<p style="color: aliceblue;font-size: 20px;">Out Patient Department</p>
+			</center>
+				<img src="images/op1.png" height="50px" width="300px">
+				<p style="color: aliceblue;font-size: 20px;">OP Time: 9 AM to 2 PM</p>
+			</div>	
+		</div>
+		<div class="box1">
+			<div class="small_content">
+				<center>
+					<p style="color: aliceblue;font-size: 20px;">Emergency Care</p>
+				</center>
+				<img src="images/Emergency (2).png" height="200px" width="300px">
+				<p style="color: aliceblue;font-size: 20px;">24 X 7 service</p>
+			</div>	
+		</div>
+	
+
+	</div>
+<!-----footer----->
+	<div class="footer" style="padding-left:30px">
+		<div class="small_content">
+		    <table >
+			<h5 style="text-color:white;font-size: 15px;float:left;margin-top: 5px;">
+          
+				<tr>
+
+					<td >Place: Puthenchira, Thrissur</td>
+				</tr>
+				 <tr>
+				 	<td>Public Hospitals</td>
+				 </tr>
+ 			     <tr>
+					 <td>Pin: 680682</td>
+				 </tr>
+				 <tr>
+				 	<td>Contcat Number:0480 2891039</td>
+				 </tr>
+			 </table>
+			</h5>
+		</div>
+	</div>
+   </div>
+</div>
+</body>
+</html>
